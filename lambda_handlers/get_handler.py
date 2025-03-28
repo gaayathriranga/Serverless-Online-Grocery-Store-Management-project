10e45aaf7d7c3d9efe889695147b4062605c01b7
Gaayathri Ranganathan <gaayathri537@gmail.com>
2025-03-28 19:06:04 +0100

    get_handler.py

---
+++ lambda_handlers/get_handler.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+    
+    table = dynamodb.Table('mygrocery-product')
+
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(data, cls=DecimalEncoder)
+    }
